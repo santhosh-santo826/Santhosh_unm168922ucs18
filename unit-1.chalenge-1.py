b985844fac2e1b13cfbf7c6e1fb232013963313f
santhosh-santo826 <144912052+santhosh-santo826@users.noreply.github.com>
2023-10-05 11:59:06 +0530

    Update unit-1.chalenge-1.py

--- unit-1.chalenge-1.py
+++ unit-1.chalenge-1.py
@@ -1,16 +1,31 @@
-#include<stdio.h>
-long int multiplyNumbers(int n);
-int main() {
-    int n;
-    printf("Enter a positive integer: ");
-    scanf("%d",&n);
-    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
-    return 0;
-}
 
-long int multiplyNumbers(int n) {
-    if (n>=1)
-        return n*multiplyNumbers(n-1);
-    else
-        return 1;
-}
+# Python 3 program to find  
+# factorial of given number 
+
+  
+# Function to find factorial of given number 
+
+def factorial(n): 
+
+       
+
+    res = 1
+
+      
+
+    for i in range(2, n+1): 
+
+        res *= i 
+
+    return res 
+
+  
+# Driver Code 
+
+num = 5; 
+
+print("Factorial of", num, "is", 
+factorial(num)) 
+
+   
+# This code is contributed by Smitha Dinesh Semwal 
